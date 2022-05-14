04bc5d8c7cdaf106531d891fccc0db6c9cdde336
SiVox <mateuszujma1999@gmail.com>
2022-05-14 22:16:53 +0200

    one

---
+++ src/Osoba.java
@@ -0,0 +1,40 @@
+public class Osoba implements Zwierze {
+    public String
+            imie,
+            zwierze,
+            przedstawSieValue;
+    public int
+            wiek,
+            nrTel;
+
+
+    public Osoba(String imie,int wiek,int nrTel,String zwierze){
+        this.imie=imie;
+        this.wiek=wiek;
+        this.nrTel=nrTel;
+        this.zwierze=zwierze;
+        DajGlos();
+    }
+
+    public String PrzedstawSie(){
+        return "Nazywam sie" +imie+" a "+przedstawSieValue;
+    }
+
+    @Override
+    public String DajGlos() {
+        przedstawSieValue="Moje zwierze to "+zwierze+" I robi "+JakieZwierze();
+        return przedstawSieValue;
+    }
+
+    public String JakieZwierze(){
+        switch (zwierze.toLowerCase()){
+            case "pies":
+                return "HauHauHau";
+            case "kot":
+                return "MaiuMaiu";
+            default:
+                return "Blad";
+
+        }
+    }
+}
